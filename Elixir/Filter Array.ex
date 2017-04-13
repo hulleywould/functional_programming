1fc32f0dea6119a0fd0023a78ca93fa4785ef5dc
sherwin <hulleywould@gmail.com>
2017-04-13 23:45:55 +0200

    Initial Commit

---
+++ Elixir/Filter Array.ex
@@ -0,0 +1,11 @@
+defmodule Solution do
+    def filter_array() do
+        num = IO.gets("") |> String.strip |> String.to_integer
+        data = IO.read(:stdio, :all) |> String.strip |> String.split
+        data
+            |> Enum.map(&(String.to_integer(&1)))
+            |> Enum.filter_map(&(&1 < num), &(Integer.to_string(&1) <> "\n"))
+            |> IO.puts
+    end
+end
+Solution.filter_array()
